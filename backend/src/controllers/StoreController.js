a43066d2b1f30f9cdecd4c78440b8676a1b27391
camposyuri <yurinapoleao@gmail.com>
2021-02-01 20:12:37 -0300

    Creating CRUD the store

--- backend/src/controllers/StoreController.js
+++ backend/src/controllers/StoreController.js
@@ -1,8 +1,69 @@
 const knex = require("../database");
 
 module.exports = {
-  async getStore(request, response) {
-    const results = await knex("store").orderBy("id");
-    return response.json(results);
+  // Select com InnerJoin que vai trazer só as interções que estão na tabela
+  async getStoreJoin(request, response, next) {
+    try {
+      const results = await knex("store")
+        .innerJoin("users", "users.id", "=", "store.user_id")
+        // Esse select pega todas os registros da store e trás o nome da tabela users
+        .select("store.*", "users.name", "users.email")
+        .orderBy("id");
+
+      return response.json(results);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async createStore(request, response, next) {
+    try {
+      const { social_reason, fantasy, cpf_cnpj, cep, user_id } = request.body;
+
+      await knex("store").insert({
+        social_reason,
+        fantasy,
+        cpf_cnpj,
+        cep,
+        user_id,
+      });
+
+      return response.status(201).send();
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async updateStore(request, response, next) {
+    try {
+      const { social_reason, fantasy, cpf_cnpj, cep, user_id } = request.body;
+      const { id } = request.params;
+
+      await knex("store")
+        .update({
+          social_reason,
+          fantasy,
+          cpf_cnpj,
+          cep,
+          user_id,
+        })
+        .where({ id });
+
+      return response.status(201).send();
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async removeStore(request, response, next) {
+    try {
+      const { id } = request.params;
+
+      await knex("store").where({ id }).del();
+
+      return response.status(201).send();
+    } catch (error) {
+      next(error);
+    }
   },
 };
